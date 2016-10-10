af187b633bb3a33f00872f0e8ab08e1e80d7b58a
mahmoud-PC\mahmoud <mahmoud_m.elkhouly@yahoo.com>
2016-10-10 16:10:11 -0500

    lab 5
    display jason data in table

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const fs = require('fs');
+const path=require("path");
+
+    app.set('view engine', 'ejs');
+    app.set('trust proxy', true);
+    app.set('x-powered-by', false);
+    app.enable('case sensitive routing');
+    app.set('strict routing', true);
+
+app.get("/inventors", function(request, response){
+
+    const invstPath = path.join(__dirname,'/inventors.json');
+
+    const inventors = JSON.parse(fs.readFileSync(invstPath, 'utf8'));
+    response.render(__dirname+'/home.ejs', {title:'Inventors', inventors:inventors});
+});
+
+app.listen(4000);
+console.log("The Server is running");
